3e15202e66e65cb1bd0a1385eabc73b53c8db58a
grocid <carl@grocid.net>
2016-07-15 20:39:52 +0200

    updated structure

--- operations/arithmetic.py
+++ operations/arithmetic.py
@@ -1,22 +1,7 @@
 import numpy as np
 import pyfftw
 
-from binascii import hexlify
 from copy import copy
-
-def to_bin(vec, length):
-    num = int(''.join([str(x) for x in list(vec)]), 2)
-    return ('%%0%dx' % (length << 1) % num).decode('hex')[-length:] # libnum
-    
-def from_bin(binary):
-    return np.array([int(x) for x in bin(int(hexlify(binary), 16))[2:]])
-    
-def to_int(vec):
-    s = ''.join(str(x) for x in vec[::-1])
-    return int(s, 2)
-
-def from_int(num):
-    return np.array([int(x) for x in bin(num)[2:]][::-1])
 
 def div_poly(x, y):
     D = np.fft.rfft(np.array(list(y) + [0] * (len(x) - len(y))))
